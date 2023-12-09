3c6cad3eb0a05e73b6c5dd9d4e2c84f772183ab0
xmomoex <moimorex@gmail.com>
2023-12-09 19:31:05 +0100

    s

---
+++ paginicil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    include("inicio.css");
+    include("navbar.css");
+    ?>
+
+</head>
+<body>
+    <?php
+    include("navbar.html");
+    include("inicio.html");
+    
+    ?>
+</body>
+</html>
