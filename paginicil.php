724ea80b2867c10d248f0a6f3e28cfe2e64adc39
xmomoex <moimorex@gmail.com>
2023-12-10 14:31:59 +0100

    s

--- paginicil.php
+++ paginicil.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php
-    include("inicio.css");
-    include("navbar.css");
-    ?>
+
 
 </head>
 <body>
